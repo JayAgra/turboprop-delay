134cec41ac37534f2199f1de61604f6bc5fcc9f9
Jayen Agrawal <dev@jayagra.com>
2026-03-26 12:37:26 -0400

    Setup

---
+++ iOS/turboprop-desynchronosis/turboprop_desynchronosisApp.swift
@@ -0,0 +1,17 @@
+//
+//  turboprop_desynchronosisApp.swift
+//  turboprop-desynchronosis
+//
+//  Created by Jayen Agrawal on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct turboprop_desynchronosisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
